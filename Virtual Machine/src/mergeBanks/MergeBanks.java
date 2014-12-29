a80f0092b3118cecc36535925a65b04afbeda51a
Chillvan <silvan.hoppler@gmail.com>
2014-12-29 18:22:33 +0100

    Test Initial

--- Virtual Machine/src/mergeBanks/MergeBanks.java
+++ Virtual Machine/src/mergeBanks/MergeBanks.java
@@ -80,18 +80,26 @@ public class MergeBanks {
                 	String initalJD = JDKunden.get(i).getVorname().substring(0, 0);
                 	String initialVCT = KundenArray.get(j).getVorname().substring(0, 0);
                 	
+                	System.out.println("Initial 1. If-Schlaufe klappt");
+                	
                 	// Falls Initialen, Nachname & komplette Adresse übereinstimmt wird gemerged
                 	if(initalJD.equals(initialVCT) &&
                         JDKunden.get(i).getNachname().equals(KundenArray.get(j).getNachname()) &&
                         JDKunden.get(i).getAdresse().equals(KundenArray.get(j).getAdresse())){
                 		
+                		System.out.println("Initial 2. If-Schlaufe klappt");
+                		
                 		// Voller Vorname wird bestimmt
                 		String name = new String();
                 		if(JDKunden.get(i).getVorname().length() > KundenArray.get(j).getVorname().length()){
                 			name = JDKunden.get(i).getVorname();
+                			
+                			System.out.println("Initial 3. If-Schlaufe klappt");
                 		}
                 		else{
                 			name = KundenArray.get(j).getVorname();
+                			
+                			System.out.println("Initial 3. If-Schlaufe klappt (else)");
                 		}
                 		neu = false;
                     	merge(KundenArray, JDKunden, JDKonti, i, j);
