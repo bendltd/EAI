852ef5e448650ef6798ca6ccbf538e8e6ffe0469
Chillvan <silvan.hoppler@gmail.com>
2014-12-31 14:49:15 +0100

    test

--- Virtual Machine/src/mergeBanks/MergeBanks.java
+++ Virtual Machine/src/mergeBanks/MergeBanks.java
@@ -59,6 +59,29 @@ public class MergeBanks {
                     break;
                 }
                 
+                // Falls Vorname, Nachname & Strassenname übereinstimmt wird gemerged, aber eine Meldung geworfen
+                else if(JDKunden.get(i).getVorname().equals(KundenArray.get(j).getVorname()) &&
+                    	JDKunden.get(i).getNachname().equals(KundenArray.get(j).getNachname()) &&
+                    	adr1[0].replace(",","").equals(adr2[0].replace(",", "")) &&
+                    	adr1[adr1.length-1].equals(adr2[adr2.length-1])){
+                	
+                	neu = false;
+                	merge(KundenArray, JDKunden, JDKonti, i, j);
+                	KundenArray.get(j).setFehler("Korrekte Hausnummer bei Kunde erfragen");
+                	KundenArray.get(j).setAdresse(adresseneu);
+                    break;
+                }
+                
+                // Falls Vor- und Nachname in umgekehrter Reihenfolge vorkommen und die Adresse stimmt, wird gemerged und eine Meldung ausgegeben
+                else if(JDKunden.get(i).getVorname().equals(KundenArray.get(j).getNachname()) &&
+                		JDKunden.get(i).getNachname().equals(KundenArray.get(j).getVorname()) &&
+                		JDKunden.get(i).getAdresse().equals(KundenArray.get(j).getAdresse())){
+                	neu = false;
+                	merge(KundenArray, JDKunden, JDKonti, i, j);
+                	KundenArray.get(j).setFehler("Vor- und Nachname m\u00F6glicherweise vertauscht");
+                    break;
+                }
+                
                 // Bei Kunden mit Initialen als Vornamen
                 else if(JDKunden.get(i).getVorname().charAt(1) == '.' || KundenArray.get(j).getVorname().charAt(1) == '.'){
                 	String initalJD = JDKunden.get(i).getVorname().substring(0, 1);
@@ -84,36 +107,11 @@ public class MergeBanks {
                 	}
                 	
                 }
-                
-                // Falls Vorname, Nachname & Strassenname übereinstimmt wird gemerged, aber eine Meldung geworfen
-                else if(JDKunden.get(i).getVorname().equals(KundenArray.get(j).getVorname()) &&
-                    	JDKunden.get(i).getNachname().equals(KundenArray.get(j).getNachname()) &&
-                    	adr1[0].replace(",","").equals(adr2[0].replace(",", "")) &&
-                    	adr1[adr1.length-1].equals(adr2[adr2.length-1])){
-                	
-                	neu = false;
-                	merge(KundenArray, JDKunden, JDKonti, i, j);
-                	KundenArray.get(j).setFehler("Korrekte Hausnummer bei Kunde erfragen");
-                	KundenArray.get(j).setAdresse(adresseneu);
-                    break;
-                }
-                	
-                
                 // Falls von den Attributen Vorname, Nachname und Adresse zwei Ã¼bereinstimmen, wird eine Meldung geworfen, dass evtl. der Kunde mehrmals erfasst wurde
                 else if((JDKunden.get(i).getVorname().equals(KundenArray.get(j).getVorname()) && JDKunden.get(i).getNachname().equals(KundenArray.get(j).getNachname())) || 
                         (JDKunden.get(i).getVorname().equals(KundenArray.get(j).getVorname()) && JDKunden.get(i).getAdresse().equals(KundenArray.get(j).getAdresse())) ||
                         (JDKunden.get(i).getAdresse().equals(KundenArray.get(j).getAdresse()) && JDKunden.get(i).getNachname().equals(KundenArray.get(j).getNachname()))){
                     JDKunden.get(i).setFehler("M\u00F6glicherweise mehrfach vorhanden");
-                    break;
-                }
-                
-                // Falls Vor- und Nachname in umgekehrter Reihenfolge vorkommen und die Adresse stimmt, wird gemerged und eine Meldung ausgegeben
-                else if(JDKunden.get(i).getVorname().equals(KundenArray.get(j).getNachname()) &&
-                		JDKunden.get(i).getNachname().equals(KundenArray.get(j).getVorname()) &&
-                		JDKunden.get(i).getAdresse().equals(KundenArray.get(j).getAdresse())){
-                	neu = false;
-                	merge(KundenArray, JDKunden, JDKonti, i, j);
-                	KundenArray.get(j).setFehler("Vor- und Nachname m\u00F6glicherweise vertauscht");
                     break;
                 }
             }
